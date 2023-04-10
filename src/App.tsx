a39fea3709f0c017a102dab344cd44137ff1bdf1
Serhii Bryk <81683502+serhiibryk@users.noreply.github.com>
2023-04-10 15:30:06 +0300

    Feat/main structure (#1)
    Feat/main structure (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+import Table from './components/Table'
+import { HeaderContainer } from './style'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'App'}>
+      {/* <header className="App-header"></header> */}
+      <main>
+        <HeaderContainer>
+          <Table />
+        </HeaderContainer>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
